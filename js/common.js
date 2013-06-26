fb7c54655e7c26e374bfd99e4903929299d868ab
liuxf2010 <liuxf2010@163.com>
2013-06-26 06:54:19 -0700

    add baidu tongji

--- js/common.js
+++ js/common.js
@@ -1,7 +1,10 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-41996665-1', 'lady172.com');
-ga('send', 'pageview');
+(function(w,d,n,h,s) {
+    w[n]=w[n]||[];
+    h = d.createElement("script");
+    h.src = "http://hm.baidu.com/hm.js?b566737c9601c4fc566afe1899cd8ccd";
+    s = d.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(h, s);
+}(this,document,'_hmt'));
 ;(function($){
 	$.define = function(name, object){
 		if(!$[name]){
