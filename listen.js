0347847ff06328801a1706eb7554752b9bd884c6
liuxf2010 <liuxf2010@163.com>
2024-07-17 09:26:14 +0800

    单词自动听写

---
+++ listen.js
@@ -0,0 +1,5 @@
+const data = [
+  { name: "Day 1", words: "key watch thing eraser dictionary crayon tomato photo potato library" },
+  { name: "Day 2", words: "bottle guitar circle tennis classmate hobby country husband tofu really" },
+  { name: "Day 3", words: "information violin mean handsome together knee activity hike chess dining#hall" },
+]
